66990af8bb5f21af5c8a6eb7c26ce38b44bbd8e6
97tlsdk <97tlsdk@naver.com>
2022-09-19 23:04:42 +0900

    stash: Posting

---
+++ client/src/pages/Community/detail/Posting.js
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Wrapper = styled.div`
+//게시글 포스팅 등록 화면
+.postingterritory{
+  margin: 20px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.pbackground{
+  width: 90vw;
+  height: 80vh;
+  padding: 20px;
+  border-radius: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  background-color: #f9f9f9;
+}
+//게시글 제목
+.posttitle{
+  width: 90vw;
+  padding: 0px 20px;
+  
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  background-color: #EEF1FF;
+}
+.title{
+  padding: 10px 20px;
+
+  font-weight: bold;
+}
+.titletext{
+  width: 80vw;
+}
+//게시글 내용
+.postcontent{
+  width: 90vw;
+  padding: 20px 20px;
+
+  display: flex;
+  flex: row;
+  justify-content: center;
+  align-items: center;
+}
+.contenttext{
+  width: 85vw;
+  height: 60vh;
+}
+//게시글 등록 버튼
+.postbutton{
+}
+.postbuttontext{
+  background-color: #EEF1FF;
+  color: #000000;
+  font-weight: bold;
+  &:hover{
+    color: #9263FF;
+  }
+}
+`
+const StyledLink = styled(Link)`
+text-decoration: none;
+
+width: 10vw;
+  margin-top: 20px;
+  padding: 20px;
+  border-radius: 20px;
+  box-shadow: 1px 2px 2px gray;
+
+  background-color: #EEF1FF;
+  text-align: center;
+
+`
+
+function Posting() {
+  return (
+    <Wrapper>
+      {/* 게시글 포스팅 등록 화면 */}
+      <div className='postingterritory'>
+        <div className='pbackground'>
+          {/* 게시글 제목 */}
+          <div className='posttitle'>
+            <span className='title'>글 제목</span>
+            <input className='titletext' type="textarea" placeholder='내용을 입력해주세요.'></input>
+          </div>
+          {/* 게시글 내용 */}
+          <div className='postcontent'>
+            <input className='contenttext' type="textarea" placeholder="내용을 입력해주세요."></input>
+          </div>
+        </div>
+        {/* 게시글 등록 버튼 */}
+        <StyledLink to={"/community"} className='postbutton'>
+          <span to={"/community"} className="postbuttontext">등록</span>
+        </StyledLink>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Posting
