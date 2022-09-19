c05bad550e703ea1145e9e62269a56669573e9c0
97tlsdk <97tlsdk@naver.com>
2022-09-19 23:04:42 +0900

    stash: Posting

---
+++ client/src/pages/Community/detail/Reply.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import React from 'react'
+
+const Wrapper = styled.div`
+
+`
+
+function Reply(props) {
+  return (
+    <Wrapper>
+        {/* 게시글 확인 화면 */}
+        <div className='replytettitory'>
+            <div className='rbackground'>
+                {/* 게시글 제목 */}
+                {/* 게시글 내용 */}
+                {/* 댓글 작성 */}
+                {/* 작성된 댓글 */}
+            </div>
+        </div>
+    </Wrapper>
+  )
+}
+
+export default Reply
