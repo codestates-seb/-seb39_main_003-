fa9d68de159b063c4443afb98c8e282f60749efd
jaekwan278 <wprhks536@gmail.com>
2022-09-29 14:50:20 +0900

    Tag 수정

--- server/src/main/java/com/web/MyPetForApp/board/repository/TagRepository.java
+++ server/src/main/java/com/web/MyPetForApp/board/repository/TagRepository.java
@@ -3,6 +3,8 @@ package com.web.MyPetForApp.board.repository;
 import com.web.MyPetForApp.board.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {
+import java.util.List;
 
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findAllByPid(int pid);
 }
