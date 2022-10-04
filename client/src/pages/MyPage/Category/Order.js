c0a0cdb20e93a6f5d181c8ae878c126b6c16f4fa
guswns3198 <guswns3198@naver.com>
2022-10-05 06:05:41 +0900

    배송지 기능 추가중....

--- client/src/pages/MyPage/Category/Order.js
+++ client/src/pages/MyPage/Category/Order.js
@@ -1,8 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router';
+import Cat from '../../Shopping/images/cat.png';
 
 
 const Wrapper = styled.div`
@@ -17,23 +17,36 @@ box-sizing: border-box;
 }
 // 주문 내역 텍스트
 .orderBackground{
-  width: 100%;
-  padding: 20px;
-  margin: 20px;
-
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: center;
   align-items: center;
 }
 .ordertext{  
   margin: 20px;
   padding: 20px;  
 
-  font-size: x-large;
+  font-size: 2rem;
   font-weight: bolder;
   text-align: right;
 }
+
+.adressBox {
+  width: 90%;
+  margin: 0px 0px 40px 0px;
+  display: flex;
+  justify-content: center;
+  border: 1px solid red;
+}
+
+.firstRadio {
+  margin-right: 15px;
+}
+
+.secondRadio {
+  margin: 0px 15px;
+}
+
 // 내 주문 목록
 .myorderlistBackground{
   display: flex;
@@ -61,37 +74,92 @@ box-sizing: border-box;
   color: #9263FF;
 }
 // 주문 목록 본문
-.myorderlistBox{
-  width: 90vw;
-  padding: 20px;
-  margin-bottom: 30px;
-  border-bottom-right-radius: 10px;
-  border-bottom-right-radius: 10px;
+.orderAll {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  /* align-items: center; */
+  /* border: 2px solid blue; */
+  background-color: #EEF1FF;
+  margin: 1rem 0rem;
+}
 
+.orderList1{
+  width: 45%;
+  padding: 20px;
+  /* margin-bottom: 30px; */
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
 
-
-  background-color: #EEF1FF;
+  /* border: 2px solid green; */
 }
-.myorderimg{}
+
+.orderList2 {
+  width: 45%;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  /* border: 2px solid red; */
+}
+
+.deliver {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1.1rem;
+}
+
+.charge {
+  font-weight: 500;
+}
+
+.charge2 {
+  font-weight: 600;
+}
+
+.myorderimg{
+  width: 15rem;
+  height: 10rem;
+  /* border: 1px solid red; */
+}
+
 .myordername{
-  width: 100%;
-  height: 5rem;
-  border: 1px solid red;
+  /* border: 1px solid red; */
+  font-size: 1.3rem;
+  font-weight: 600;
+  margin: 0px 20px;
 }
 .myorderstate{
-  border: 1px solid red;
-  width: 100%;
-  height: 5rem;
+  /* border: 1px solid red; */
+  font-size: 1.2rem;
+  font-weight: 500;
 }
 `
 
-function Order() {
+function Order( {convertPrice} ) {
 
   const [orderList, setOrderList] = useState([]);
+  const [selectValue, setSelectValue] = useState("내 배송지");
+
+  const handleChangeMy = (e) => {
+    // console.log(`*****handleChange*****`);
+    // console.log(`선택한 값 : ${e.target.value}`);
+
+    setSelectValue("내 배송지");
+  };
+
+  const handleChangeNew = (e) => {
+    // console.log(`*****handleChange*****`);
+    // console.log(`선택한 값 : ${e.target.value}`);
+
+    setSelectValue("신규 배송지");
+  };
+
   const location = useLocation();
 
   // console.log(location)
@@ -99,28 +167,85 @@ function Order() {
     setOrderList(location.state.list)
   }, [orderList])
 
+  const token = sessionStorage.getItem('accessToken');
+  const realToken = token.slice(7)
+  // console.log(realToken)
+  
+  window.Buffer = window.Buffer || require("buffer").Buffer; 
+  
+  const base64Payload = realToken.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
+  const payload = Buffer.from(base64Payload, 'base64'); 
+  const result = JSON.parse(payload.toString())
+  // console.log(result);
+
+    const [info, setInfo] = useState([]);
+    
+    React.useEffect(() => {
+      fetch(`http://211.58.40.128:8080/api/v1/member/${result.id}`)
+      .then(res => res.json())
+      .then(res => {
+        setInfo(res)
+        // console.log(res)
+      })
+    } , [])
+
   return (
     <Wrapper>
       <div className='orderterritory'>
         {/* 주문내역 텍스트 */}
         <div className='orderBackground'>
-          <div className='ordertext'>내 주문 내역</div>
+          <div className='ordertext'>주문서</div>
         </div>
+
+        <div className="adressBox">
+            <input
+              name="platform"
+              type="radio"
+              className='firstRadio'
+              // checked={setSelectValue("내 배송지")}
+              onChange={handleChangeMy}
+              />
+            내 배송지
+            <input
+              name="platform"
+              type="radio"
+              className='secondRadio'
+              // checked={setSelectValue("신규 배송지")}
+              onChange={handleChangeNew}
+            />
+            신규 배송지
+         </div>
+
+         <div>
+           { selectValue === "내 배송지" ? <span>{info.address}</span> : <span>새로 들어갈 주소</span> }
+         </div>
+
         {/* 내 주문 목록 */}
         <div className='myorderlistBackground'>
           {/* 주문 목록 탭 */}
           <div className='myordertabBackground'>
               <span className='myordertab'>상품정보</span>
-              <span className='myordertab'>주문상태</span>
+              <span className='myordertab'>배송비 / 배송 형태</span>
           </div>
           {/* 주문목록 본문 */}
 
           {orderList && orderList.map((el, idx) => {
             return (
-              <div className='myorderlistBox' key={idx}>
-                  {/* <img className='myorderimg'/> */}
-                  <span className='myordername'>{el.itemName}</span>
-                  <span className='myorderstate'>{el.price * el.itemCnt}</span>
+              <div className='orderAll' key={idx}>
+                <div className='orderList1'>
+                    <span>
+                      <img src={Cat} alt='상품 사진' className='myorderimg'/>
+                    </span>
+                    <span className='myordername'>{el.itemName}</span>
+                    <span className='myorderstate'>{convertPrice(el.price * el.itemCnt)} 원</span>
+                </div>
+
+                <div className='orderList2'>
+                  <span className='deliver'>
+                    <span className='charge'>택배배송</span>
+                    <span className='charge2'>배송비 무료</span>
+                  </span>
+                </div>  
               </div>
             )
           })}
