58589e7697677126b51da31f7851d6d1691513ce
guswns3198 <guswns3198@naver.com>
2022-10-05 06:03:29 +0900

    필요없는 줄 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,6 @@ import FAQPost from "./pages/FAQ/FAQPost";
 
 function App() {
 
-    const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
   
     const convertPrice = (price) => {
@@ -62,7 +61,7 @@ function App() {
                 <Route path="/mypage/cart" element={<Cart cart={cart} setCart={setCart} convertPrice={convertPrice}/>} />
                 <Route path="/mypage/cart" element={<CartBody cart={cart} setCart={setCart} convertPrice={convertPrice}/>} />
                 <Route path="/mypage/wish" element={<Wish convertPrice={convertPrice} />} />
-                <Route path="/mypage/order" element={<Order/>} />
+                <Route path="/mypage/order" element={<Order convertPrice={convertPrice} />} />
 
                 <Route path="/community/walk" element={<Community />} />
                 <Route path="/community/info" element={<Info />} />
