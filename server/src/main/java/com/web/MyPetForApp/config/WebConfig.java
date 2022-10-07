086e801161b3d3238c7afb233ec489a74bdc0d43
mirea70 <deae670@daum.net>
2022-10-07 13:42:42 +0900

    배포된 애플리케이서션 통신 Test8(cors ,제발)

--- server/src/main/java/com/web/MyPetForApp/config/WebConfig.java
+++ server/src/main/java/com/web/MyPetForApp/config/WebConfig.java
@@ -14,9 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
 ////                .allowedOrigins("http://localhost:3000") // 해당 origin 허용
 ////                .allowCredentials(true)
 //                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://seb39-main-003-jv5wjh03r-nomga.vercel.app" )
-                .allowedHeaders("*");
-//                .exposedHeaders("Authorization")
-//                .exposedHeaders("refresh")
-//                .allowedMethods("GET", "POST", "PATCH", "DELETE"); // 허용할 Request 메서드
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .exposedHeaders("refresh")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE"); // 허용할 Request 메서드
     }
 }
