6b9f2ed530fbf472f64c7f77f95f35acc443a1a9
mirea70 <deae670@daum.net>
2022-10-10 20:29:04 +0900

    발표 BE 최종본3

--- server/src/main/java/com/web/MyPetForApp/config/WebConfig.java
+++ server/src/main/java/com/web/MyPetForApp/config/WebConfig.java
@@ -1,22 +1,22 @@
-package com.web.MyPetForApp.config;
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로
-////                .allowedOrigins("http://localhost:3000") // 해당 origin 허용
-                .allowCredentials(true)
-                .allowedOrigins("https://seb39-main-003-bsihsudrk-nomga.vercel.app/")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization")
-                .exposedHeaders("refresh")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE"); // 허용할 Request 메서드
-    }
-}
+//package com.web.MyPetForApp.config;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // 모든 경로
+//////                .allowedOrigins("http://localhost:3000") // 해당 origin 허용
+//                .allowCredentials(true)
+//                .allowedOrigins("https://seb39-main-003-bsihsudrk-nomga.vercel.app/")
+//                .allowedHeaders("*")
+//                .exposedHeaders("Authorization")
+//                .exposedHeaders("refresh")
+//                .allowedMethods("GET", "POST", "PATCH", "DELETE"); // 허용할 Request 메서드
+//    }
+//}
